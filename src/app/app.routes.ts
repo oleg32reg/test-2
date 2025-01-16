908dac6faaeb95a26cb850dbe6049faf601d763e
OR <oleg32reg.log@gmail.com>
2025-01-16 17:51:44 +0300

    add 404 page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { PageNotFoundComponent } from '@components/page-not-found/page-not-found.component';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+  {
+    path: '',
+    loadComponent: () =>
+      import('@pages/main-page/main-page.component').then((c) => c.MainPageComponent),
+    title: 'Список пользователей | TEST',
+    children: [],
+  },
+  {
+    path: '**',
+    component: PageNotFoundComponent,
+    title: 'Страница не найдена',
+    data: { cssClass: 'ng-page-404' },
+  },
+];
